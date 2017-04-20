4a6104befdfdc572a78957f3461673365c4db7d5
viktorkrskv0 <viktorkrskv0@gmail.com>
2017-04-20 19:59:20 +0300

    deleted the SP_MSG enum
    it is soppouse to be in the h file

--- SPConfig.c
+++ SPConfig.c
@@ -15,21 +15,6 @@ char* sysVarsControl[numberOfArguements] = {"spImagesDirectory","spImagesPrefix"
 int sysVarsFlag[numberOfArguements] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0};
 int currentVarIndex;
 
-typedef enum sp_config_msg_t {
-	SP_CONFIG_MISSING_DIR,
-	SP_CONFIG_MISSING_PREFIX,
-	SP_CONFIG_MISSING_SUFFIX,
-	SP_CONFIG_MISSING_NUM_IMAGES,
-	SP_CONFIG_CANNOT_OPEN_FILE,
-	SP_CONFIG_ALLOC_FAIL,
-	SP_CONFIG_INVALID_INTEGER,
-	SP_CONFIG_INVALID_STRING,
-	SP_CONFIG_INVALID_ARGUMENT,
-	SP_CONFIG_INDEX_OUT_OF_RANGE,
-	SP_CONFIG_NOT_A_VARIABLE,
-	SP_CONFIG_SUCCESS
-} SP_CONFIG_MSG;
-
 /** A type used for defining the configuration file**/
 typedef struct sp_config_t{
 	char* spImagesDirectory;
