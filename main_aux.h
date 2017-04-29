68960b3e1ecfeb65980d730494a27da463ba3747
KiKi <guy@kikiscene.com>
2017-04-29 15:18:01 +0300

    Update main_aux.h

--- main_aux.h
+++ main_aux.h
@@ -5,11 +5,21 @@
  *      Author: gleit
  */
 
-
 #ifndef MAIN_AUX_H_
 #define MAIN_AUX_H_
 #include <stdbool.h>
+#include "unit_tests\\unit_test_util.h"
+#include "SPConfig.h"
+#include "SPLogger.h"
 
-//bool spManageMSG(SP_CONFIG_MSG msg, SPConfig config);
+void runTests();
+
+char* getConfigFileName(int argc, char** agrv);
+
+bool manageCMSG(SP_CONFIG_MSG* Cmsg);
+
+bool manageLMSG(SP_LOGGER_MSG* Smsg);
+
+void freeAll(SPConfig config, SPLogger logger);
 
 #endif /* MAIN_AUX_H_ */
