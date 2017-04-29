ef2bf3066c00c819ffaf09291376d91302be0e62
viktorkrskv0 <viktorkrskv0@gmail.com>
2017-04-29 15:14:17 +0300

    all the tests that I did
    still missing some some functions tests

--- unit_tests/kd_tree_unit_test.c
+++ unit_tests/kd_tree_unit_test.c
@@ -200,7 +200,130 @@ static bool InitTreeTest() {
 	return true;
 }
 
+static bool concatTest() {
+	char* s1 = "Hello";
+	char* s2 = "World";
+	char* s3 = concat(s1,s2);
+	ASSERT_TRUE(strcmp(s3,"HelloWorld") == 0);
+	ASSERT_TRUE(strlen(s3)==10);
+	return true;
+}
+
+static bool convertIntToStrTest(){
+	int num1 = 123;
+	int num2 = 3;
+	int num3 = 1234567890;
+	char* str1 = convertIntToStr(num1);
+	char* str2 = convertIntToStr(num2);
+	char* str3 = convertIntToStr(num3);
+	ASSERT_TRUE(strcmp(str1,"123") == 0);
+	ASSERT_TRUE(strcmp(str2,"3") == 0);
+	ASSERT_TRUE(strcmp(str3,"1234567890") == 0);
+	free(str1);
+	free(str2);
+	free(str3);
+	return true;
+}
+
+static bool buildAddressTest(){
+	publicConfig.spImagesDirectory = "images/";
+	publicConfig.spImagesPrefix = "img";
+	publicConfig.spImagesSuffix = ".jpg";
+	char* address1 = buildAddress(1);
+	char* address2 = buildAddress(2);
+	char* address3 = buildAddress(3);
+	ASSERT_TRUE(strcmp(address1,"images/img1.jpg") == 0);
+	ASSERT_TRUE(strcmp(address2,"images/img2.jpg") == 0);
+	ASSERT_TRUE(strcmp(address3,"images/img3.jpg") == 0);
+	free(address1);
+	free(address2);
+	free(address3);
+	return true;
+}
+
+static bool buildFeatAddressTest(){
+	char* address1 = buildFeatAddress(1);
+	char* address2 = buildFeatAddress(2);
+	char* address3 = buildFeatAddress(3);
+	ASSERT_TRUE(strcmp(address1,"images/img1.feats") == 0);
+	ASSERT_TRUE(strcmp(address2,"images/img2.feats") == 0);
+	ASSERT_TRUE(strcmp(address3,"images/img3.feats") == 0);
+	free(address1);
+	free(address2);
+	free(address3);
+	return true;
+}
+
+static bool writeFeatsToFileTest(){
+	//allocating five points
+	//point arr is the features
+	int i;
+	int j;
+	FILE* f_feats = fopen("f_feats.txt","w+");
+	SPPoint** pointArr = (SPPoint**)malloc(sizeof(SPPoint*)*5);
+	for(i = 0; i < 5; i++){
+		double* data = (double*)malloc(sizeof(double)*5);
+		for(j = 0; j < 5; j++){
+			data[j] =  i*j;
+		}
+		pointArr[i] = spPointCreate(data,5,2);
+	}
+	writeFeatsToFile(f_feats,pointArr,2,5);
+//	identicalFiles("f_feats.txt","f_test.txt") ? printf("identical\n") : printf("not indentical\n");
+//DELETE AFTER
+	ASSERT_FALSE(identicalFiles("f_feats.txt","f_test.txt"));
+	for(i = 0; i < 5; i++){
+		spPointDestroy(pointArr[i]);
+	}
+	free(pointArr);
+	return true;
+}
+
+static bool InitArrayTest(){
+	//this test depend on a pre-written feats files
+	publicConfig.spNumOfImages = 3;
+	publicConfig.spPCADimension = 3;
+	SPPoint** pointArr = (SPPoint**)malloc(sizeof(SPPoint*)*9);
+	KD_ARRAY kdArr = InitArray(pointArr,9);
+	SPPoint** pointArrTest = (SPPoint**)malloc(sizeof(SPPoint*)*9);
+	double data1[] = {0.000,1.000,2.000};
+	double data2[] = {3.000,4.000,5.000};
+	double data3[] = {6.000,7.000,8.000};
+	double data4[] = {0.000,1.000,2.000};
+	double data5[] = {3.000,4.000,5.000};
+	double data6[] = {6.000,7.000,8.000};
+	double data7[] = {0.000,1.000,2.000};
+	double data8[] = {3.000,4.000,5.000};
+	double data9[] = {6.000,7.000,8.000};
+	pointArrTest[0] = spPointCreate(data1,3,1);
+	pointArrTest[1] = spPointCreate(data2,3,1);
+	pointArrTest[2] = spPointCreate(data3,3,1);
+	pointArrTest[3] = spPointCreate(data4,3,2);
+	pointArrTest[4] = spPointCreate(data5,3,2);
+	pointArrTest[5] = spPointCreate(data6,3,2);
+	pointArrTest[6] = spPointCreate(data7,3,3);
+	pointArrTest[7] = spPointCreate(data8,3,3);
+	pointArrTest[8] = spPointCreate(data9,3,3);
+	ASSERT_TRUE(spPointL2SquaredDistance(pointArrTest[0],kdArr.arr[0])==0);
+	ASSERT_TRUE(spPointL2SquaredDistance(pointArrTest[1],kdArr.arr[1])==0);
+	ASSERT_TRUE(spPointL2SquaredDistance(pointArrTest[2],kdArr.arr[2])==0);
+	ASSERT_TRUE(spPointL2SquaredDistance(pointArrTest[3],kdArr.arr[3])==0);
+	ASSERT_TRUE(spPointL2SquaredDistance(pointArrTest[4],kdArr.arr[4])==0);
+	ASSERT_TRUE(spPointL2SquaredDistance(pointArrTest[5],kdArr.arr[5])==0);
+	ASSERT_TRUE(spPointL2SquaredDistance(pointArrTest[6],kdArr.arr[6])==0);
+	ASSERT_TRUE(spPointL2SquaredDistance(pointArrTest[7],kdArr.arr[7])==0);
+	ASSERT_TRUE(spPointL2SquaredDistance(pointArrTest[8],kdArr.arr[8])==0);
+	//isIdentical()
+	return true;
+}
+
 int mainTreeTest(){
+	RUN_TEST(concatTest);
+	RUN_TEST(convertIntToStrTest);
+	RUN_TEST(buildAddressTest);
+	RUN_TEST(buildFeatAddressTest);
+	RUN_TEST(writeFeatsToFileTest);
+	RUN_TEST(InitArrayTest);
 	RUN_TEST(SplitTest);
 	RUN_TEST(InitTreeTest);
 	return 0;
